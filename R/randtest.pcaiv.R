47adee27bb28e78e0042c2a3193ddfc5a6ab0904
Aurélie Siberchicot <aurelie.siberchicot@univ-lyon1.fr>
2017-08-09 19:31:18 +0000

    version 1.7-8

--- R/randtest.pcaiv.R
+++ R/randtest.pcaiv.R
@@ -5,25 +5,23 @@
         stop("Type 'pcaiv' expected")
     appel <- as.list(xtest$call)
     dudi1 <- eval.parent(appel$dudi)
-    df <- as.data.frame(eval.parent(appel$df))
+    df <- data.frame(eval.parent(appel$df))
     y <- as.matrix(dudi1$tab)
     inertot <- sum(dudi1$eig)
     sqlw <- sqrt(dudi1$lw)
     sqcw <- sqrt(dudi1$cw)
-    
        
-    fmla <- as.formula(paste("y ~", paste(dimnames(df)[[2]], collapse = "+")))
-    mf <- model.frame(fmla,data=cbind.data.frame(y,df))
-    mt <- attr(mf,"terms")
-    x <- model.matrix(mt,mf)
+    fmla <- as.formula(paste("y ~", paste(names(df)[[2]], collapse = "+")))
+    mf <- model.frame(fmla, data = cbind.data.frame(y, df))
+    mt <- attr(mf, "terms")
+    x <- model.matrix(mt, mf)
     wt <- outer(sqlw, sqcw)
     ## Fast function for computing sum of squares of the fitted table
     obs <- sum((lm.wfit(y = y,x = x, w = dudi1$lw)$fitted.values * wt)^2) / inertot
 
-    isim <- c()
+    isim <- rep(NA, nrepet)
     for(i in 1:nrepet)
-      isim[i] <- sum((lm.wfit(y = y,x = x[sample(nrow(x)),], w = dudi1$lw)$fitted.values * wt)^2) / inertot
+      isim[i] <- sum((lm.wfit(y = y, x = x[sample(nrow(x)), ], w = dudi1$lw)$fitted.values * wt)^2) / inertot
     return(as.randtest(sim = isim, obs = obs, call = match.call(), ...))
-    
   }
 
